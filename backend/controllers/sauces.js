188bdeb17d391b72e8233cf29bb76bb2328fcc5e
bno4 <bnduchemin@gmail.com>
2024-02-16 14:35:03 +0100

    back to loacl prod

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -24,10 +24,10 @@ exports.createSauce = (req, res, next) => {
   const sauce = new Sauce({
     ...sauceObject,
     userId: req.auth.userId,
-    // imageUrl: `${req.protocol}://${req.get("host")}/images/${
-    //   req.file.filename
-    // }`,
-    imageUrl: `${process.env.BASE_URL}/images/${req.file.filename}`,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    // imageUrl: `${process.env.BASE_URL}/images/${req.file.filename}`,
   });
 
   sauce
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     return this.http.post<{ message: string }>(
-      'https://hot-takes-production.up.railway.app/api/auth/signup',
+      'http://localhost:3000/api/auth/signup',
       { email: email, password: password }
     );
   }
@@ -31,7 +31,7 @@ export class AuthService {
   loginUser(email: string, password: string) {
     return this.http
       .post<{ userId: string; token: string }>(
-        'https://hot-takes-production.up.railway.app/api/auth/login',
+        'http://localhost:3000/api/auth/login',
         { email: email, password: password }
       )
       .pipe(
